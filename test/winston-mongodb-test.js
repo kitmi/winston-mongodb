ea9327e6a5dab1e02433b790e8686caecd613b12
Rockie Guo <rockie@kitmi.com.au>
2018-10-09 20:10:18 +1100

    Fix several bugs caused by the latest mongodb driver

--- test/winston-mongodb-test.js
+++ test/winston-mongodb-test.js
@@ -4,6 +4,7 @@
  * @license MIT
  * @author charlie@nodejitsu.com (Charlie Robbins)
  * @author 0@39.yt (Yurij Mikhalevich)
+ * @author rockie@kitmi.com.au (Rockie Guo)
  */
 'use strict';
 const mongodb = require('mongodb');
@@ -13,14 +14,15 @@ const test_suite = require('abstract-winston-transport');
 const MongoDB = require('../lib/winston-mongodb').MongoDB;
 
 const dbUrl = process.env.USER_WINSTON_MONGODB_URL
-    ||process.env.WINSTON_MONGODB_URL||'mongodb://localhost:27017/winston';
+    ||process.env.WINSTON_MONGODB_URL|| 'mongodb://localhost:27017/winston'; //'mongodb://root:root@localhost:27017/winston?authSource=admin';
 
-mongoose.connect(dbUrl);
+mongoose.connect(dbUrl, { useNewUrlParser: true});
+let client;
 
 test_suite({name: '{db: url}', Transport: MongoDB, construct: {db: dbUrl}});
 test_suite({name: '{db: url} on capped collection', Transport: MongoDB,
     construct: {db: dbUrl, capped: true, collection: 'cappedLog'}});
 test_suite({name: '{db: client promise}', Transport: MongoDB,
-    construct: {db: mongodb.MongoClient.connect(dbUrl, {useNewUrlParser: true})}});
+    construct: {db: mongodb.MongoClient.connect(dbUrl, {useNewUrlParser: true}).then( c => { return (client = c); } )}, after: (options, done) => { client.close(done); }});
 test_suite({name: '{db: mongoose client}', Transport: MongoDB,
-    construct: {db: mongoose.connection}});
+    construct: {db: mongoose.connection}, after: (options, done) => { mongoose.connection.close(done); } });
